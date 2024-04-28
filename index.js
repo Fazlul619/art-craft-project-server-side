2dbe2199f7f1b639da05aae2efe2952b9d5c9ac6
Fazlul Alam <fazlulalam619@gmail.com>
2024-04-29 01:24:09 +0600

    Data Pass in Database

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 require("dotenv").config();
 const cors = require("cors");
 app.use(cors());
+app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5000; // or any port of your choice
-
-// assignment-10
-// W4A8QFrF29P42PEs
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.crkhnnq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -25,6 +25,19 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const craftCollection = client.db("craftDB").collection("craft");
+    app.get("/addCraftItems", async (req, res) => {
+      const cursor = craftCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/addCraftItems", async (req, res) => {
+      const newCraftItem = req.body;
+      console.log(newCraftItem);
+      const result = await craftCollection.insertOne(newCraftItem);
+      res.send(result);
+    });
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
